059ceb9b3fab32590f2b5708403e75783b98975a
Patrick Berryhill <pjberry@gmail.com>
2013-02-11 05:33:41 -0600

    forgot a test!

--- src/test/scala/com/bitmotif/elementize/StringParts_UT.scala
+++ src/test/scala/com/bitmotif/elementize/StringParts_UT.scala
@@ -1,10 +1,23 @@
 package com.bitmotif.elementize
 
+import org.scalatest.FunSpec
+
 /**
  * User: pjberry
  * Date: 2/10/13
  * Time: 11:01 AM
  */
-class StringParts_UT {
+class StringParts_UT extends FunSpec {
+
+  it("should ") {
+    val string = "abcdef"
+    val substring = Substring("de", 3)
+
+    val stringParts = StringParts(string, substring)
+
+    assert(stringParts.prefix === "abc")
+    assert(stringParts.theString === "De")
+    assert(stringParts.suffix === "f")
+  }
 
 }
