2b0595d7e08f17eb799fe7f3cf5a468d60f47895
Patrick Berryhill <pjberry@gmail.com>
2013-02-14 06:01:11 -0600

    moving the elements into a package object

--- src/main/scala/com/bitmotif/elementize/NameDecoration.scala
+++ src/main/scala/com/bitmotif/elementize/NameDecoration.scala
@@ -13,10 +13,6 @@ variable names across the board
 */
 class NameDecoration {
 
-  val elementAbbreviations = List("H","He","Li","Be","B","C","N","O","F","Ne","Na","Mg","Al","Si","P","S","Cl","Ar","K","Ca","Sc","Ti","V","Cr","Mn","Fe","Co","Ni","Cu","Zn","Ga","Ge","As","Se","Br","Kr","Rb","Sr","Y","Zr","Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd","In","Sn","Sb","Te","I","Xe","Cs","Ba","La","Ce","Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu","Hf","Ta","W","Re","Os","Ir","Pt","Au","Hg","Tl","Pb","Bi","Po","At","Rn","Fr","Ra","Ac","Th","Pa","U","Np","Pu","Am","Cm","Bk","Cf","Es","Fm","Md","No","Lr","Rf","Db","Sg","Bh","Hs","Mt","Ds","Rg","Cn","Uut","Fl","Uup","Lv","Uus","Uuo")
-
-  val lowerCaseElementAbbreviations = elementAbbreviations.map(_.toLowerCase)
-
   private val MAX_ABBREVIATION_SIZE: Int = 2
 
   private val capitalizeString: Substring => String = x => x.value.head.toUpper + x.value.tail.mkString
